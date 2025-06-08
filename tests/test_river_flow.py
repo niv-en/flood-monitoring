1fb71bd4c00db29a932dacdd88889fcc08fba511
nivenrooprah <niven.rooprah@bbc.co.uk>
2025-06-08 23:00:42 +0100

    updated readme

--- tests/test_river_flow.py
+++ tests/test_river_flow.py
@@ -1,13 +1,8 @@
 from flood_monitoring import RiverFlow 
-import pytest 
+
 
 VALID_ID = '2928TH' 
 
-# @pytest.fixture
-# def valid_obj() -> station:
-#     test = station(VALID_ID) 
-
-#     return test
 
 def test_instantiation():
 
